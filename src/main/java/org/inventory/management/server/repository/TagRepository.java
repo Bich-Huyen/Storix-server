ebdd05996adb4e2a3af8016ad1c929173023ba37
hienlephan2003 <hienlephan2003@gmail.com>
2024-12-22 18:08:01 +0700

    add TagRepository, StockReportRepository, StockReportDetailRepository

--- src/main/java/org/inventory/management/server/repository/TagRepository.java
+++ src/main/java/org/inventory/management/server/repository/TagRepository.java
@@ -1,6 +1,7 @@
 package org.inventory.management.server.repository;
 
 import org.inventory.management.server.entity.Product;
+import org.inventory.management.server.entity.Tag;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -9,7 +10,7 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-public interface ProductRepository extends JpaRepository<Product, Long> {
-    Page<Product> findByNameContainingIgnoreCase(String name, Pageable pageable);
-    List<Product> findByIsDeletedIsNullOrIsDeletedFalse();
+public interface TagRepository extends JpaRepository<Tag, Long> {
+    Page<Tag> findByNameContainingIgnoreCase(String name, Pageable pageable);
+    List<Tag> findByIsDeletedFalse();
 }
